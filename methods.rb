b852ca37cce791b65c7eb142d2eff785f9295e4a
Amos Langat <amos.langat@student.moringaschool.com>
2022-10-17 16:28:52 +0300

    complete assignment

--- methods.rb
+++ methods.rb
@@ -1,1 +1,31 @@
 # Your code here!
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+
+def greet(name)
+    puts "Hello, #{name}!"
+end
+greet("Amos")
+
+
+def greet_with_default(name = "programmer") #has default value
+    puts "Hello, #{name}!"
+end
+greet_with_default
+
+
+def add(x, y)
+    return x + y
+end
+
+
+def halve(x)
+    if x.is_a?(Integer) #check if x is an integer
+        return x/2  
+    else
+        return nil
+    end
+end
+
